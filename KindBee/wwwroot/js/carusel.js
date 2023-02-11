5ffe299cdc6adc7cf3be7c523cda59b7d81a7e55
Дмитрий Венчиков <DmitriVenchikov@outlook.com>
2023-02-12 00:07:43 +0300

    edit

---
+++ KindBee/wwwroot/js/carusel.js
@@ -0,0 +1,33 @@
+﻿
+
+
+window.onload = function () {
+    window.scrollTo(0, +localStorage.getItem('page_scroll'));
+
+   
+    document.addEventListener('scroll', function () {
+        localStorage.setItem('page_scroll', window.pageYOffset);
+    });
+}
+
+
+var angle = 0;
+function galleryspin(sign) {
+    spinner = document.querySelector("#spinner");
+    if (!sign) { angle = angle + 45; } else { angle = angle - 45; }
+    spinner.setAttribute("style", "-webkit-transform: rotateY(" + angle + "deg); -moz-transform: rotateY(" + angle + "deg); transform: rotateY(" + angle + "deg);");
+}
+function sleep(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+async function fun1() {
+    while (true) {
+        await sleep(2000);
+        angle = angle + 45;
+        spinner = document.querySelector("#spinner");
+        spinner.setAttribute("style", "-webkit-transform: rotateY(" + angle + "deg); -moz-transform: rotateY(" + angle + "deg); transform: rotateY(" + angle + "deg);");
+    }
+
+}
+
+fun1();
--- KindBee/wwwroot/js/site.js
+++ KindBee/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+window.onload = function () {
+    window.scrollTo(0, +localStorage.getItem('page_scroll'));
+
+
+    document.addEventListener('scroll', function () {
+        localStorage.setItem('page_scroll', window.pageYOffset);
+    });
+}
